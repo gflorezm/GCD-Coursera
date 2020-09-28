457dd8c7bcb59cf40a0f9ea8e4d629f7da040835
Guillermo L. Florez Montero <gflorezmontero@gmail.com>
2020-09-28 15:58:46 -0300

    Initial commit

---
+++ Run_analysis.R
@@ -0,0 +1,11 @@
+#########################################################################
+#                                                                       #
+#             Getting and Cleaning Data Project                         #
+#             Coursera - Jhons Hopkins University                       #
+#             Author: Guillermo Florez Montero                          #
+#                                                                       #
+#########################################################################
+
+
+
+# See "Readme" for additional information
